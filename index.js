e2da4daf7968c6ef124352a4252f49c1fff27a29
Adrien Chantrel <adrien.chantrel@isen.yncrea.fr>
2020-06-02 17:10:51 +0200

    Added Mongoose locally

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+
 const express = require('express'); 
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (err) => {
+    console.log(err);
+});
+db.once('open', () => {
+    console.log("Connected to Database !")
+});
 
 app.listen(8080, () => {
     console.log("SERVER ON");
