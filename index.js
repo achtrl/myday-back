6c49eb164c87b6ebd54a85d53c9bba328aa6a666
Adrien Chantrel <adrien.chantrel@isen.yncrea.fr>
2020-06-03 17:28:06 +0200

    Added Google connexion - Still have to receive user info from frontend and stocked them in the database

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'); 
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', (err) => {
-    console.log(err);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-db.once('open', () => {
-    console.log("Connected to Database !")
+const db = mongoose.connection;
+db.on("error", (err) => {
+  console.log(err);
+});
+db.once("open", () => {
+  console.log("Connected to Database !");
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const googleRouter = require("./src/google-util");
+app.use("/google", googleRouter);
+
 app.listen(8080, () => {
-    console.log("SERVER ON");
-})
+  console.log("SERVER ON");
+});
